56a507d10991172f73dbe36eb76b8932958b82fc
Lennart Kotzur <len.kot@icloud.com>
2026-05-16 15:06:52 +0200

    feat: expand vim-opts with modern defaults
    * modernize tab settings from vim.cmd("set ...") to vim.opt.*
    * add line numbers (number + relativenumber)
    * add search opts (ignorecase, smartcase, hlsearch=false, incsearch)
    * add scrolloff=8, signcolumn=yes, no wrap, splitright, splitbelow
    * add clipboard=unnamedplus and undofile for persistent undo
    * add termguicolors, updatetime=250, timeoutlen=300

--- nvim/lua/vim-opts.lua
+++ nvim/lua/vim-opts.lua
@@ -1,12 +1,37 @@
 -- vim-opts
 
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
+-- Leader keys (must precede lazy.nvim bootstrap)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+-- Indentation
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Scroll / layout
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Clipboard and undo
+vim.opt.clipboard = "unnamedplus"
+vim.opt.undofile = true
+
+-- UI
+vim.opt.termguicolors = true
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
